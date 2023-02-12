61ecd0a3b0502978c81eaf046faef96783801638
Mayur Chavan <msrcp7@gmail.com>
2023-02-12 16:01:34 +0530

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return (
+      <div
+        style={{
+          textAlign: "center",
+          marginBottom: 10,
+        }}
+      >
+        Made with ♥ by{" "}
+      
+          Mayur Chavan
+      
+      </div>
+    );
+  };
+  
+  export default Footer;
